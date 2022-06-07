ae00ae5c83c390b0d539855f65c3f3843a734dad
Tom Boettger <t.boettger@live.de>
2022-06-07 12:15:21 +0200

    Implement an http server and create dedicated test functions

--- benchmark/index.mjs
+++ benchmark/index.mjs
@@ -1,12 +1,24 @@
+import http from "http";
 import { PrismaClient } from "@prisma/client";
-import mysql from "mysql2/promise";
+import mysql from "mysql2";
 import { findUnique, findMany } from "../dist/index.js";
+
+const host = "localhost";
+const port = 7744;
 
 const prisma = new PrismaClient();
 
-console.log(await prisma.customer.findUnique({ where: { customer_id: 15 } }));
+const client = mysql
+  .createPool({
+    database: "sakila",
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "safe",
+  })
+  .promise();
 
-const sql = findUnique(
+const findUniqueQuery = findUnique(
   "customer",
   [
     "customer_id",
@@ -19,14 +31,37 @@ const sql = findUnique(
     "create_date",
     "last_update",
   ],
-  "customer.customer_id = 15"
+  "customer.customer_id = ?"
 ).compile();
 
-const client = await mysql.createConnection(
-  "mysql://root:safe@localhost:3306/sakila"
-);
+async function findUniqueSqlToJson(customerId) {
+  const [rows] = await client.query(findUniqueQuery, [customerId]);
+  return rows[0]._json;
+}
 
-const [rows, fields] = await client.query(sql);
-console.log(rows[0]._json);
+async function findUniquePrisma(customerId) {
+  return await prisma.customer.findUnique({
+    where: { customer_id: customerId },
+  });
+}
 
-await client.end();
+const server = http.createServer(async (req, res) => {
+  const customerId = Math.floor(Math.random() * 25) + 1;
+  const body = JSON.stringify(await findUniquePrisma(customerId));
+  //const body = JSON.stringify(await findUniqueSqlToJson(customerId));
+  res
+    .writeHead(200, {
+      "Content-Length": Buffer.byteLength(body),
+      "Content-Type": "application/json",
+    })
+    .end(body);
+});
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
+
+server.on("close", async () => {
+  await prisma.$disconnect();
+  await client.end();
+});
